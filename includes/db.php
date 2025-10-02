f46ff96bf94d16fe52bf1273d9bbc8654552e577
sha@1012 <ranishrama131012@gmail.com>
2025-10-02 12:28:00 +0530

    learnhub project

--- includes/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Database connection - UPDATE these values to match your environment
-$host = 'localhost';
-$user = 'root';
-$pass = '';
-$dbname = 'leranhub';
-$conn = new mysqli($host,$user,$pass,$dbname);
-if($conn->connect_error) die('DB Connection error: '.$conn->connect_error);
